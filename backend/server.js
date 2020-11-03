72fb055aabea1e50b0aef19698de04cbdad66a96
Tarik nasraoui <tariik.nasraoui@gmail.com>
2020-11-03 12:56:20 +0100

    add errorMiddleWare

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDb from "./config/db.js";
+import { notFound, errorHandler } from "./middleWare/errorMiddleWare.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -11,14 +12,10 @@ app.get("/", (req, res) => {
   res.send("API running ....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
