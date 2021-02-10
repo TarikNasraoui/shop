9befc4ffeaf7c711b362b0e8caf6fff56403c1be
Tarik nasraoui <tariik.nasraoui@gmail.com>
2021-02-10 11:58:56 +0100

    prepare to deploy

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,7 +13,6 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
   const count = await Product.countDocuments({ ...keyword });
-  console.log(pageSize * (page - 1));
   const products = await Product.find({ ...keyword })
     .limit(pageSize)
     .skip(pageSize * (page - 1));
